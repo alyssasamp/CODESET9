027f2c414cb9d1ab528de7567ac9fe193d768bf5
alyssasamp <134331900+alyssasamp@users.noreply.github.com>
2023-07-24 22:13:39 -0400

    beginning code with module

---
+++ courseinfo.py
@@ -0,0 +1,21 @@
+room = {
+    "CS101": 3004,
+    "CS102": 4501,
+    "CS103": 6755,
+    "NT110": 1244,
+    "CM241": 1411
+}
+instructor = {
+    "CS101": "Haynes",
+    "CS102": "Alvarado",
+    "CS103": "Rich",
+    "NT110": "Burke",
+    "CM241": "Lee"
+}
+meeting_time = {
+    "CS101": "8:00 am",
+    "CS102": "9:00 am",
+    "CS103": "10:00 am",
+    "NT110": "11:00 am",
+    "CM241": "1:00 pm"
+}
